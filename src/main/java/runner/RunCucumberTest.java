26cfdbfd2b6d0dca3eeeeb3ed3626d87276b7934
Violetta Korostashovets <violettakorostashovets@gmail>
2017-12-06 15:23:17 +0200

    log4j properties

--- src/main/java/runner/RunCucumberTest.java
+++ src/main/java/runner/RunCucumberTest.java
@@ -1,16 +1,15 @@
 package runner;
 
-
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/main/java/cucumber/Settings1",
+        features = "src/main/java/cucumber/Settings1_IncomingCalls.feature",
         glue = {"com.citrix.grasshopper.at.steps"},
         //tags = {"@Settings"},
-        strict=false
+        strict = false
 )
 public class RunCucumberTest {
 }
